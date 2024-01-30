41fc55826fa1bf043b5fcde5712694efb0c8677f
jamiefayezsksk <@jimejamie76@gmail.com>
2024-01-30 23:15:13 +0800

    newly added

---
+++ src/components/Patient/PatientAppointment.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Card } from "antd";
+
+
+
+function PatientAppointment() {
+  return (
+    <div>
+      <Card title="Welcome to Patient">
+        <h3>
+         Appointments
+        </h3>
+      </Card>
+    </div>
+    
+  );
+}
+
+export default PatientAppointment;
