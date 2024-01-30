ee9756421ac746a9962f09770e4d7594fd29dcb6
jamiefayezsksk <@jimejamie76@gmail.com>
2024-01-30 23:13:17 +0800

    newly added

--- src/components/Patient/PatientHome.jsx
+++ src/components/Patient/PatientHome.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Card } from "antd";
 
 
+
 function PatientHome() {
   return (
     <div>
-      <Card title="Welcome to Your Dashboard">
+      <Card title="Welcome Patient">
         <h3>
-          Hello, Patient! This is your personalized dashboard where you can
-          view and manage your appointments, medical records, and more.
+         Hello, Patient! This is your personalized dashboard where you can
+         view and manage your appointments, medical records, and more.
         </h3>
-        {/* Add more content and features specific to the patient dashboard */}
       </Card>
     </div>
+    
   );
 }
 
