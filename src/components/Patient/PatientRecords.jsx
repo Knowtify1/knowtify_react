e8ced976c6a599f5790d2afcfe30ad99acc3dc89
jamiefayezsksk <@jimejamie76@gmail.com>
2024-01-30 23:15:36 +0800

    newly added

---
+++ src/components/Patient/PatientRecords.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Card } from "antd";
+
+
+
+function PatientRecords() {
+  return (
+    <div>
+      <Card title="Welcome to Patient">
+        <h3>
+         Records
+        </h3>
+      </Card>
+    </div>
+    
+  );
+}
+
+export default PatientRecords;
