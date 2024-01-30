ffac90752a0ac62cf3e3e77b39210b068edab334
jamiefayezsksk <@jimejamie76@gmail.com>
2024-01-30 22:52:00 +0800

    added patient dashboard

--- src/PatientDashboard.jsx
+++ src/PatientDashboard.jsx
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "antd";
+import { ConfigProvider, Layout } from "antd";
+import { ToggleThemeButton } from "./components/ToggleThemeButton";
+import { Outlet } from "react-router-dom";
+import knowtifylogov2 from "./assets/icon.ico";
 
-function PatientDashboard() {
+
+import PatientMenu from "./components/Patient/PatientMenu";
+
+const { Header, Sider } = Layout; 
+
+export function PatientDashboard() {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [collapsed, setCollapsed] = React.useState(false);
+  
+
+  const logo = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#fff",
+    padding: "10px",
+  };
+
+  const logoIconStyle = {
+    width: "55px",
+    height: "55px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "1.5rem",
+    borderRadius: "50%",
+    // background: "rgba(28, 17, 41, 0.88)",
+  };
+
+  const toggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
+
+
   return (
-    <div>
-      <Card title="Welcome to Your Dashboard">
-        <h3>
-          Hello, Patient! This is your personalized dashboard where you can
-          view and manage your appointments, medical records, and more.
-        </h3>
-        {/* Add more content and features specific to the patient dashboard */}
-      </Card>
-    </div>
+    <ConfigProvider theme={{}}>
+      <Layout>
+        <Sider
+          theme={darkTheme ? "dark" : "light"}
+          collapsed={collapsed}
+          width={200}
+        >
+          <div style={logo}>
+            <div style={logoIconStyle}>
+              <img src={knowtifylogov2} alt="knowtifylogo" className="" />
+            </div>
+          </div>
+          <PatientMenu />
+        </Sider>
+        <Layout>
+          <div>
+            <Outlet />
+          </div>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
   );
 }
 
 export default PatientDashboard;
+
