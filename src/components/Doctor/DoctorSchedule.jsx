90a4379dfdaee06747ec82954f015d7adda42b91
jamiefayezsksk <@jimejamie76@gmail.com>
2024-01-30 23:10:27 +0800

    container mx-auto

--- src/components/Doctor/DoctorSchedule.jsx
+++ src/components/Doctor/DoctorSchedule.jsx
@@ -4,21 +4,16 @@ import DoctorCalendar from "./Components/DoctorCalendar";
 
 function DoctorSchedule() {
   return (
-    <div className="container mx-auto p-4">
-      <div className="flex flex-col gap-4">
+    <div className="container mx-auto p-0">
+      <div className="flex flex-col gap-0 h-10">
         <Card
           title={
-            <h3 className="text-3xl font-semibold text-center ">Calendar</h3>
+            <h3 className="text-3xl font-semibold text-center">Calendar</h3>
           }
+          className="container mx-auto p-0" // Allow the card to take up remaining vertical space
         >
           <DoctorCalendar />
         </Card>
-
-        <Card
-          title={<h3 className="text-3xl font-semibold text-center"></h3>}
-        ></Card>
-
-        {/* Add more cards as needed */}
       </div>
     </div>
   );
