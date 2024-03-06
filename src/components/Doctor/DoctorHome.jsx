c0f82e43bb3f2d2795a2c9722c1016ffde13fba5
jamiefayezsksk <@jimejamie76@gmail.com>
2024-03-06 20:40:55 +0800

    <DoctorSetSpecialty />

--- src/components/Doctor/DoctorHome.jsx
+++ src/components/Doctor/DoctorHome.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Card, Calendar } from "antd";
-
+import DoctorSetSpecialty from "./Components/DoctorSetSpecialty";
 import DoctorOverview from "../Doctor/Components/DoctorOverView";
 
 function DoctorHome() {
   return (
-    <div className="flex">
-      {/* Overview Section */}
-      <Card
-        className="pl-8 pr-4 pb-5 pt-2 custom-card"
-        style={{ width: "100%", height: "auto" }}
-      >
-        <DoctorOverview />
-      </Card>
-    </div>
+    <>
+      <DoctorSetSpecialty />
+      <div className="flex">
+        {/* Overview Section */}
+        <Card
+          className="pl-8 pr-4 pb-5 pt-2 custom-card"
+          style={{ width: "100%", height: "auto" }}
+        >
+          <DoctorOverview />
+        </Card>
+      </div>
+    </>
   );
 }
 
